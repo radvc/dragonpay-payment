64aff237f503496746380ee21173dfd9b4f0590e
Ryan Angelo <cadagryanangelo@gmail.com>
2017-08-21 17:08:17 +0800

    setup configuration

--- lib/dragonpay_payment.rb
+++ lib/dragonpay_payment.rb
@@ -1,5 +1,17 @@
-require "dragonpay_payment/version"
+require 'dragonpay_payment/configuration'
+require 'dragonpay_payment/merchant'
+require 'dragonpay_payment/version'
 
 module DragonpayPayment
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
 end
